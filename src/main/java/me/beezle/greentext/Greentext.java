0e40fd5326d737ebfc4db28215cb66382d7c8d2f
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-06-07 08:45:48 +0300

    Update system prints

--- src/main/java/me/beezle/greentext/Greentext.java
+++ src/main/java/me/beezle/greentext/Greentext.java
@@ -7,11 +7,11 @@ public class Greentext extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new PlayerHandler(), this);
-        System.out.print("[Greentext] Enabled.");
+        System.out.print("[Greentext v1.0.0] Enabled.");
     }
 
     @Override
     public void onDisable() {
-        System.out.print("[Greentext] Disabled.");
+        System.out.print("[Greentext v1.0.0] Disabled.");
     }
 }
